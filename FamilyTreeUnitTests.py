30e5d736c4de4015182d701a66d22211c23fa197
Subramanian Venkatesan <subramanian.bsv@gmail.com>
2014-04-06 20:44:40 -0400

    Code cleanup in the unittest

--- FamilyTreeUnitTests.py
+++ FamilyTreeUnitTests.py
@@ -8,45 +8,57 @@ class FamilyTreeBaseClass(unittest.TestCase):
 
 class FamilyTreeUnitTest(FamilyTreeBaseClass):
 	def testaddPersons(self):
+
+		# Test 1
 		self.ftree.addPerson(None)
 		assert len(self.ftree.adjList.keys()) == 0,"None should not be added"
 
+		# Test 2
 		p = Person("Subbu","Venkatesan")
 		self.ftree.addPerson(p)
 		assert len(self.ftree.adjList.keys()) == 1,"Person not added"
 
-
+		# Test 3
 		self.ftree.addPerson(p)
 		assert len(self.ftree.adjList.keys()) == 1,"Duplicate person added"
 
+		# Test 4
 		p = Person("S","V")
 		self.ftree.addPerson(p)
 		assert len(self.ftree.adjList.keys()) == 2,"Unable to add a person with the same name"
 
 	def testsetRootParent(self):
+
+		# Test 1
 		p = Person("S","V")
 		self.ftree.setRootParent(p)
 		assert hasattr(self.ftree, 'rootParent')==False,"Person not in family, but still its the root parent"
 
+		# Test 2
 		self.ftree.addPerson(p)
 		self.ftree.setRootParent(p)
 		assert self.ftree.rootParent==p,"Unable to set the root parent"
 
 	def testfindParent(self):
+
+		# Test 1
 		p = Person("S","V")
 		self.ftree.addPerson(p)
 		parent = self.ftree.findParent(p)
 		assert parent == None,"Parent must be none"
 
+		# Test 2
 		parent = self.ftree.findParent(None)
 		assert parent == None,"Parent must be none"
 
+		# Test 3
 		o = Person("Subbu","V")
 		self.ftree.addPerson(o)
 		self.ftree.connectPersons(p,o)
 		parent = self.ftree.findParent(o)
 		assert parent == p,"Parent is wrong"
 
+		# Test 4
 		x = Person("X","Y")
 		self.ftree.addPerson(x)
 		self.ftree.connectPersons(o,x)
@@ -54,6 +66,8 @@ class FamilyTreeUnitTest(FamilyTreeBaseClass):
 		assert parent == o,"Parent is wrong"
 
 	def testconnectPerson(self):
+		
+		# Test 1
 		p = Person("S","V")
 		self.ftree.addPerson(p)
 		o = Person("Subbu","V")
@@ -61,34 +75,40 @@ class FamilyTreeUnitTest(FamilyTreeBaseClass):
 		self.ftree.connectPersons(p,o)
 		assert self.ftree.findParent(o) == p,"ConnectPersons is wrong"
 
+		# Test 2
 		self.ftree.connectPersons(o,o)
 		assert self.ftree.findParent(o)!=o,"Should not connect the same person"
 
+		# Test 3
 		x = Person("x","z")
 		self.ftree.addPerson(x)
 		self.ftree.connectPersons(x,o)
 		assert self.ftree.findParent(o) == x,"ConnectPersons is wrong"
 
 	def testgetGrandParentNode(self):
+
+		# Test 1
 		grandParent = self.ftree.getGrandParentNode(None)
 		assert grandParent==None,"GrandParent should be none"
 
+		# Test 2
 		p = Person("S","V")
 		self.ftree.addPerson(p)
 		o = Person("Subbu","V")
 		self.ftree.addPerson(o)
 		self.ftree.connectPersons(p,o)
-
 		grandParent = self.ftree.getGrandParentNode(p)
 		assert grandParent==None,"GrandParent should be none"
 
+		# Test 3
 		grandParent = self.ftree.getGrandParentNode(o)
 		assert grandParent==None,"GrandParent should be none"
 
+		# Test 4
 		grandParent = self.ftree.getGrandParentNode(p)
 		assert grandParent==None,"GrandParent should be none"
 
-
+		# Test 5
 		x = Person("x","z")
 		self.ftree.addPerson(x)
 		self.ftree.connectPersons(o,x)
@@ -96,37 +116,36 @@ class FamilyTreeUnitTest(FamilyTreeBaseClass):
 		assert grandParent==p,"GrandParent Method is wrong"
 
 	def testremovePersonAndChildren(self):
+
+		# Test 1
 		c = len(self.ftree.adjList.keys())
 		self.ftree.removePersonAndChildren(None)
 		assert c==len(self.ftree.adjList.keys()),"Nothing should be removed when None is passed"
 
-		#
+		# Test 2
 		p = Person("S","V")
 		self.ftree.addPerson(p)
 		o = Person("Subbu","V")
 		self.ftree.addPerson(o)
 		self.ftree.connectPersons(p,o)
-
 		x = Person("x","z")
 		self.ftree.addPerson(x)
 		self.ftree.connectPersons(o,x)
-
 		children = self.ftree.adjList[p]
 		self.ftree.removePersonAndChildren(p)
 		assert o not in self.ftree.adjList,"Person not removed"
 		for child in children:
 			assert child not in self.ftree.adjList,"Children not removed"
 
+		# Test 3
 		p = Person("S","V")
 		self.ftree.addPerson(p)
 		o = Person("Subbu","V")
 		self.ftree.addPerson(o)
 		self.ftree.connectPersons(p,o)
-
 		x = Person("x","z")
 		self.ftree.addPerson(x)
 		self.ftree.connectPersons(o,x)
-
 		children = self.ftree.adjList[o]
 		self.ftree.removePersonAndChildren(o)
 		assert o not in self.ftree.adjList,"Person not removed"
--- PersonUnitTests.py
+++ PersonUnitTests.py
@@ -7,36 +7,46 @@ class PersonUnitTestBaseClass(unittest.TestCase):
 
 class PersonUnitTest(PersonUnitTestBaseClass):
   def testsetFirstName(self):
-
+    #Test 1
     self.person.setFirstName("Subbu")
     assert self.person.firstName == "Subbu", "Incorrect first name"
-    
+
+    #Test 2
     self.person.setFirstName(None)
     assert self.person.firstName != None ,"Should not set None"
-
+    
+    #Test 3
     self.person.setFirstName(23)
     assert isinstance(self.person.firstName,str) == True
-
+    
+    #Test 4
     self.person.setFirstName("")
     assert len(self.person.firstName) > 0,"Length of the string must be greater than 0" 
   
   def testsetLastName(self):
+   
+    #Test 1
     self.person.setLastName("Subbu")
     assert self.person.lastName == "Subbu", "Incorrect first name"
     
+    #Test 2
     self.person.setLastName(None)
     assert self.person.lastName != None ,"Should not set None"
 
+    #Test 3
     self.person.setLastName(23)
     assert isinstance(self.person.lastName,str) == True
 
+    #Test 4
     self.person.setLastName("")
     assert len(self.person.lastName) > 0,"Length of the string must be greater than 0" 
 
   def testHashFunction(self):
+    #Test 1
     assert self.person.__hash__() == hash((self.person.firstName,self.person.lastName)),"Hash function is wrong"
 
   def testStringFunction(self):
+    #Test 1
     assert self.person.__str__() == self.person.firstName + " " + self.person.lastName,"str is wrong"
 
 if __name__ == "__main__":
